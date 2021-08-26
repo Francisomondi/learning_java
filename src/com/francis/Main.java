35596ea7552b178f63ffe713ef4a87c7fdc647cb
Francis omondi <franisomondi17@gmail.com>
2021-08-26 13:23:32 +0300

    irst comit

---
+++ src/com/francis/Main.java
@@ -0,0 +1,34 @@
+package com.francis;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("this the first line of code");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Kindly Input your age: ");
+        int age = scan.nextInt();
+        //System.out.println("your age is :" + age);
+
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("Kindly Input your first Name: ");
+        String StringfirstName = scan1.nextLine();
+        //System.out.println("your first name is :" + StringfirstName);
+
+        Scanner scan2 = new Scanner(System.in);
+        System.out.println("how long do you need before we kick you out?: ");
+        double timeLeft = scan2.nextDouble();
+
+        System.out.println("your first name is : " + StringfirstName + " of age " + age +
+                "  and you have " + timeLeft + " seconds to leave this place");
+
+
+             int numbe1 = 34;
+        int numb2 = 40;
+
+        int z = numb2 + numbe1;
+        System.out.println(z);
+
+    }
+}
